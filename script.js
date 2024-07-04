d0cc15c655fe59952b5a9e9f43d2df3ce1326c53
saaymary <sushchik.maria@gmail.com>
2024-07-04 20:18:22 +0300

    создала все элементы js по комментариям

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const sendInput = document.querySelector('#input');
+const messagesContainer = document.querySelector('.input-wrapper');
+const itemsContainer = document.querySelector('#items');
+
+sendInput.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        console.log('Сообщение отправлено');
+
+        const messageText = sendInput.value;
+
+        const newMessage = document.createElement('div');
+        newMessage.textContent = messageText;
+
+        if (messageText != '') {
+            itemsContainer.append(newMessage);
+        }
+
+        sendInput.value = '';
+
+        newMessage.addEventListener('click', function() {
+            newMessage.classList.toggle('done');
+        })
+
+    }
+})
+
 
 /* Пустые элементы не должны добавляться */
 
